92ec6b12fc1de6ff9844ffe4daa44277a2c9c5a3
pra325 <61564607+pra325@users.noreply.github.com>
2020-03-31 10:47:40 -0500

    devi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello world ! from github.com");
-        System.out.print("pratiksha subedi");
+        System.out.print("pratiksha devi subedi");
     }
 }
